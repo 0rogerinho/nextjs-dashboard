0a55506634529474a0200053d341f37af850ef7a
0rogerinho <luaansilva11@gmail.com>
2023-11-02 14:00:58 -0300

    dsa

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Lusitana as Lusitania } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitania = Lusitania({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
